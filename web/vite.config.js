ed28acfe736e47fde6b5c5474c823dea8b4a9749
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-05-23 16:40:22 +0200

    🧪 base '/'

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/selfSound/',
+  base: '/',
   css: {
     preprocessorOptions: {
       scss: {
