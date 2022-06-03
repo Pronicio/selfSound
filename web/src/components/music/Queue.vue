f9faad11403f8d7d3149c9ce5d0d2c34bb26b368
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-06-03 20:43:24 +0200

    🐛 v-for key fix

--- web/src/components/music/Queue.vue
+++ web/src/components/music/Queue.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>Queue : </h3>
-  <div v-for="item in store.queue" key="item.id" :id="item.trackId">
+  <div v-for="item in store.queue" :key="item.id" :id="item.trackId">
     <img :src="item.album.cover.big" width="50" alt="Album cover"/>
     <p>- {{ item.title }}</p>
   </div>
