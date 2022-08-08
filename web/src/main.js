9944952551461ed620c7fc2fc934cbc8cdff6148
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-08-08 11:33:32 +0200

    🩹 sorted alphabetically

--- web/src/main.js
+++ web/src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from "./router/index"
 import { createPinia } from 'pinia'
+import router from "./router/index"
 
 import mitt from 'mitt'
 const eventBus = mitt()
 
 const app = createApp(App);
 
-app.use(router)
 app.use(createPinia())
+app.use(router)
 app.config.globalProperties.eventBus = eventBus
 app.mount('#app');
