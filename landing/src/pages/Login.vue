7fbb715215c5fedb9b09519bc95ea02ead2a5c69
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-09-10 12:42:27 +0200

    🧱 Login page: connection with api.

--- landing/src/pages/Login.vue
+++ landing/src/pages/Login.vue
@@ -7,6 +7,12 @@
       <h2>See what is going on with your business</h2>
       <button><img class="google" src="../assets/images/google.png" alt="Connect with google" width="25"/>Continue with Google</button>
       <p>------------- or Sign in with Email ------------- </p>
+      <div class="error" v-if="error">
+        <p>Error: Your credentials are incorrect, please try again...</p>
+      </div>
+      <div class="success" v-if="success">
+        <p>Success: Connecting...</p>
+      </div>
       <form v-on:submit.prevent="login">
         <label for="email">Email</label>
         <input v-model="email" type="email" name="email" id="email" placeholder="mail@provider.com" required>
@@ -22,18 +28,40 @@
 
 <script>
 import { useHead } from '@vueuse/head'
+import axios from 'axios'
 
 export default {
   name: "Login",
   data: function () {
     return {
       email: null,
-      password: null
+      password: null,
+      error: false,
+      success: false
     }
   },
   methods: {
-    login: function () {
-      console.log(this.email, this.password)
+    login: async function () {
+      this.error = false;
+      this.success = false;
+
+      const req = await axios({
+        method: 'post',
+        url: `${import.meta.env.VITE_BACK}/auth/login`,
+        data: {
+          email: this.email,
+          password: this.password
+        }
+      })
+
+      const data = req.data;
+
+      if (!data.error) {
+        this.success = true;
+        console.log(data.token)
+      } else {
+        this.error = true;
+      }
     }
   },
   setup() {
