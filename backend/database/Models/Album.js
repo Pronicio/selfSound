4e8127e56e09e62524dc76504f3152f6addaa715
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-12-15 20:00:12 +0100

    💥Clean the back folder for the new infrastructure.

--- backend/database/Models/Album.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AlbumSchema = new mongoose.Schema({
-    id: { type: Number },
-    title: { type: String, required: true },
-    imageCode: { type: String }
-});
-
-module.exports = mongoose.model('Album', AlbumSchema);
--- backend/database/Models/Artist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ArtistSchema = new mongoose.Schema({
-    id: { type: Number },
-    name: { type: String, required: true },
-    imageCode: { type: String }
-});
-
-module.exports = mongoose.model('Artist', ArtistSchema);
